96c10c995ec76467323decf4b99f806f67f683d2
MallisOG <mallisiscool@gmail.com>
2019-07-29 16:50:27 -0400

    Single class for compatability in plugins, simply copy and paste the class.

--- src/com/DisabledMallis/FBLib/API/LavaVersion.java
+++
@@ -1,6 +0,0 @@
-package com.DisabledMallis.FBLib.API;
-
-public enum LavaVersion {
-	MC1_14_4,
-	MC1_12_2
-}
--- src/com/DisabledMallis/FBLib/Log.java
+++
@@ -1,6 +0,0 @@
-package com.DisabledMallis.FBLib;
-public class Log {
-	public Log(Object o) {
-		System.out.println("[§bFBLib§r] " + o);
-	}
-}
