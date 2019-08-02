da9e0ef84980c1e9fff4f8639480d837f44cd8c8
Mike <mallisiscool@gmail.com>
2019-08-02 13:02:25 -0400

    Update Hybrid.java

--- src/com/DisabledMallis/HybridLib/Hybrid.java
+++ src/com/DisabledMallis/HybridLib/Hybrid.java
@@ -14,53 +14,55 @@ import org.bukkit.Bukkit;
    }
  */
 public class Hybrid {
-	boolean Kettle;
-	boolean Lava14;
-	boolean Lava;
-	public class Main {
-		public Main() {
-			new Log("Loading ForgeBukkitLib...");
-			new Log("Server version: " + Bukkit.getVersion());
-			new Log("Bukkit version: " + Bukkit.getBukkitVersion());
+	static boolean Kettle;
+	static boolean Lava14;
+	static boolean Lava;
+	
+	public static void enableLib() {
+		new Log("Loading ForgeBukkitLib...");
+		new Log("Server version: " + Bukkit.getVersion());
+		new Log("Bukkit version: " + Bukkit.getBukkitVersion());
+		try {
+			Class.forName("net.minecraftforge.common.ForgeVersion");
+			new Log("Found Forge!");
+			new Log("Checking for Kettle...");
 			try {
-				Class.forName("net.minecraftforge.common.ForgeVersion");
-				new Log("Found Forge!");
-				new Log("Checking for Kettle...");
-				try {
-					Class.forName("kettlefoundation.kettle.KettleVersionCommand");
-					new Log("Kettle found!");
-					Kettle = true;
-				}
-				catch(ClassNotFoundException ex) {
-					new Log("Couldn't find Kettle.");
-					Kettle = false;
-				}
-				new Log("Checking for LavaPowered 1.14.4...");
-				try {
-					Class.forName("org.lavapowered.lava.LavaInternal");
-					new Log("LavaPowered 1.14.4 found!");
-					Lava14 = true;
-				}
-				catch(ClassNotFoundException ex) {
-					new Log("Couldn't find LavaPowered 1.14.4.");
-					Lava14 = false;
-				}
-				new Log("Checking for Lava...");
-				if(Bukkit.getVersion().contains("Lava")) {
-					new Log("LavaPowered found!");
-					Lava = true;
-				} else {
-					new Log("Couldn't find LavaPowered.");
-					Lava = false;
-				}
-				if(Kettle == false && Lava14 == false && Lava == false) {
-					new Log("Unknown Forge Bukkit hybrid.");
-				}
+				Class.forName("kettlefoundation.kettle.KettleVersionCommand");
+				new Log("Kettle found!");
+				Kettle = true;
 			}
 			catch(ClassNotFoundException ex) {
-				new Log("Couldn't find Forge.");
+				new Log("Couldn't find Kettle.");
+				Kettle = false;
+			}
+			new Log("Checking for LavaPowered 1.14.4...");
+			try {
+				Class.forName("org.lavapowered.lava.LavaInternal");
+				new Log("LavaPowered 1.14.4 found!");
+				Lava14 = true;
+			}
+			catch(ClassNotFoundException ex) {
+				new Log("Couldn't find LavaPowered 1.14.4.");
+				Lava14 = false;
+			}
+			new Log("Checking for Lava...");
+			if(Bukkit.getVersion().contains("Lava")) {
+				new Log("LavaPowered found!");
+				Lava = true;
+			} else {
+				new Log("Couldn't find LavaPowered.");
+				Lava = false;
+			}
+			if(Kettle == false && Lava14 == false && Lava == false) {
+				new Log("Unknown Forge Bukkit hybrid.");
 			}
 		}
+		catch(ClassNotFoundException ex) {
+			new Log("Couldn't find Forge.");
+		}
+	}
+	
+	public Hybrid() {
 	}
 	
 	public class HBL {
@@ -79,9 +81,9 @@ public class Hybrid {
 		}
 	}
 	
-	public class Log {
+	public static class Log {
 		public Log(Object o) {
-			System.out.println("[§bHybridLib§r] " + o);
+			System.out.println("[Â§bHybridLibÂ§r] " + o);
 		}
 	}
 	public enum LavaVersion {
